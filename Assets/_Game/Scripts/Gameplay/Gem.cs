56a79add8b57cf494ff1e72dc501367d7ca61825
Nermin Shawkat <shawkat.nermin@gmail.com>
2023-11-05 04:55:35 +0200

    final push

--- Assets/_Game/Scripts/Gameplay/Gem.cs
+++ Assets/_Game/Scripts/Gameplay/Gem.cs
@@ -1,8 +1,15 @@
-﻿using UnityEngine;
+﻿using System;
+using DG.Tweening;
+using UnityEngine;
 
 public class Gem : MonoBehaviour
 {
     [field:SerializeField] public GemType  Type { get; private set; }
+
+    private void Start()
+    {
+        transform.DOScale(1.2f, 0.1f).SetLoops(2, LoopType.Yoyo);
+    }
 }
 
 public enum GemType
--- Assets/_Game/Scripts/Gameplay/Managers/LevelGenerator.cs
+++ Assets/_Game/Scripts/Gameplay/Managers/LevelGenerator.cs
@@ -1,10 +1,9 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
+    private const int MinSolvablePossibilities = 3;
+
     //cell size is 1x1
     [SerializeField] private GemsCollection _gemsCollection;
     [SerializeField] private Vector2Int _gridSize;
@@ -20,9 +19,9 @@ public class LevelGenerator : MonoBehaviour
     void Generate()
     {
         _gems = new Gem[_gridSize.y, _gridSize.x];
-        
-        Vector2 position = new Vector2( (_gridSize.x-1)/-2f,  (_gridSize.y-1)/-2f);
-        
+
+        Vector2 position = new Vector2((_gridSize.x - 1) / -2f, (_gridSize.y - 1) / -2f);
+
         for (int i = 0; i < _gridSize.y; i++)
         {
             for (int j = 0; j < _gridSize.x; j++)
@@ -32,27 +31,160 @@ public class LevelGenerator : MonoBehaviour
                 {
                     gem = _gemsCollection.RandomItem;
                 }
+
                 _gems[i, j] = Instantiate(gem);
                 _gems[i, j].transform.position = position;
                 position.x++;
             }
+
             position.y++;
-            position.x = (_gridSize.x-1)/-2f;
+            position.x = (_gridSize.x - 1) / -2f;
         }
+
+        if (!HasEnoughSolutions()) Regenerate();
     }
 
     bool HasSimilarGems(Gem gem, int row, int col)
     {
-        if (col >= 2 && _gems[row, col-1].Type == gem.Type && _gems[row, col-1].Type == gem.Type)
+        if (col >= 2 && _gems[row, col - 1].Type == gem.Type && _gems[row, col - 1].Type == gem.Type)
         {
             return true;
         }
 
-        if (row >= 2 && _gems[row-1, col].Type == gem.Type && _gems[row-2, col].Type == gem.Type)
+        if (row >= 2 && _gems[row - 1, col].Type == gem.Type && _gems[row - 2, col].Type == gem.Type)
         {
             return true;
         }
 
+        return false;
+    }
+
+    bool HasEnoughSolutions()
+    {
+        int solutions = 0;
+        bool[,] takenTiles = new bool[_gridSize.y, _gridSize.x];
+        for (int i = 0; i < _gridSize.y; i++)
+        {
+            for (int j = 0; j < _gridSize.x; j++)
+            {
+                if (takenTiles[i, j]) continue;
+
+                var type = _gems[i, j].Type;
+                if (j < _gridSize.x - 2)
+                {
+                    if (i < _gridSize.y - 1)
+                    {
+                        if (!takenTiles[i, j + 1] && !takenTiles[i + 1, j + 2] && _gems[i, j + 1].Type == type &&
+                            _gems[i + 1, j + 2].Type == type)
+                        {
+                            takenTiles[i, j + 1] = true;
+                            takenTiles[i + 1, j + 2] = true;
+                            takenTiles[i, j] = true;
+
+                            solutions++;
+                            if (solutions >= MinSolvablePossibilities) return true;
+                            continue;
+                        }
+
+                        if (!takenTiles[i + 1, j + 1] && !takenTiles[i, j + 2] && _gems[i + 1, j + 1].Type == type &&
+                            _gems[i, j + 2].Type == type)
+                        {
+                            takenTiles[i + 1, j + 1] = true;
+                            takenTiles[i, j + 2] = true;
+                            takenTiles[i, j] = true;
+
+                            solutions++;
+                            if (solutions >= MinSolvablePossibilities) return true;
+                            continue;
+                        }
+                    }
+
+                    if (i > 0)
+                    {
+                        if (!takenTiles[i, j + 1] && !takenTiles[i - 1, j + 2] && _gems[i, j + 1].Type == type &&
+                            _gems[i - 1, j + 2].Type == type)
+                        {
+                            takenTiles[i, j + 1] = true;
+                            takenTiles[i - 1, j + 2] = true;
+                            takenTiles[i, j] = true;
+
+                            solutions++;
+                            if (solutions >= MinSolvablePossibilities) return true;
+                            continue;
+                        }
+
+                        if (!takenTiles[i - 1, j + 1] && !takenTiles[i, j + 2] &&
+                            _gems[i - 1, j + 1].Type == type && _gems[i, j + 2].Type == type)
+                        {
+                            takenTiles[i - 1, j + 1] = true;
+                            takenTiles[i, j + 2] = true;
+                            takenTiles[i, j] = true;
+
+                            solutions++;
+                            if (solutions >= MinSolvablePossibilities) return true;
+                        }
+                    }
+                }
+
+                if (i < _gridSize.y - 2)
+                {
+                    if (j < _gridSize.x - 1)
+                    {
+                        if (!takenTiles[i + 1, j] && !takenTiles[i + 2, j + 1] && _gems[i + 1, j].Type == type &&
+                            _gems[i + 2, j + 1].Type == type)
+                        {
+                            takenTiles[i + 1, j] = true;
+                            takenTiles[i + 2, j + 1] = true;
+                            takenTiles[i, j] = true;
+
+                            solutions++;
+                            if (solutions >= MinSolvablePossibilities) return true;
+                            continue;
+                        }
+
+                        if (!takenTiles[i + 1, j + 1] && !takenTiles[i + 2, j] && _gems[i + 1, j + 1].Type == type &&
+                            _gems[i + 2, j].Type == type)
+                        {
+                            takenTiles[i + 1, j + 1] = true;
+                            takenTiles[i + 2, j] = true;
+                            takenTiles[i, j] = true;
+
+                            solutions++;
+                            if (solutions >= MinSolvablePossibilities) return true;
+                            continue;
+                        }
+                    }
+
+                    if (j > 0)
+                    {
+                        if (!takenTiles[i + 1, j] && !takenTiles[i + 2, j - 1] && _gems[i + 1, j].Type == type &&
+                            _gems[i + 2, j - 1].Type == type)
+                        {
+                            takenTiles[i + 1, j] = true;
+                            takenTiles[i + 2, j - 1] = true;
+                            takenTiles[i, j] = true;
+
+                            solutions++;
+                            if (solutions >= MinSolvablePossibilities) return true;
+                            continue;
+                        }
+
+                        if (!takenTiles[i + 1, j - 1] && !takenTiles[i + 2, j] &&
+                            _gems[i + 1, j - 1].Type == type && _gems[i + 2, j].Type == type)
+                        {
+                            takenTiles[i + 1, j - 1] = true;
+                            takenTiles[i + 2, j] = true;
+                            takenTiles[i, j] = true;
+
+                            solutions++;
+                            if (solutions >= MinSolvablePossibilities) return true;
+                        }
+                    }
+                }
+            }
+        }
+
+        Debug.Log("regenerate");
         return false;
     }
 
@@ -65,9 +197,7 @@ public class LevelGenerator : MonoBehaviour
                 Destroy(_gems[i, j].gameObject);
             }
         }
-        
+
         Generate();
     }
-    
-    
 }
