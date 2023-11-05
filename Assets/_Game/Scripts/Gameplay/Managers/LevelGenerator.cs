407f41ece435964687651cbf27b00ddb88f93ee0
Nermin Shawkat <shawkat.nermin@gmail.com>
2023-11-05 03:56:02 +0200

    Added Regenerate Button

--- Assets/_Game/Scripts/Gameplay/Managers/LevelGenerator.cs
+++ Assets/_Game/Scripts/Gameplay/Managers/LevelGenerator.cs
@@ -27,13 +27,46 @@ public class LevelGenerator : MonoBehaviour
         {
             for (int j = 0; j < _gridSize.x; j++)
             {
-                _gems[i, j] = Instantiate(_gemsCollection.RandomItem);
+                var gem = _gemsCollection.RandomItem;
+                while (HasSimilarGems(gem, i, j))
+                {
+                    gem = _gemsCollection.RandomItem;
+                }
+                _gems[i, j] = Instantiate(gem);
                 _gems[i, j].transform.position = position;
                 position.x++;
             }
             position.y++;
             position.x = (_gridSize.x-1)/-2f;
         }
+    }
+
+    bool HasSimilarGems(Gem gem, int row, int col)
+    {
+        if (col >= 2 && _gems[row, col-1].Type == gem.Type && _gems[row, col-1].Type == gem.Type)
+        {
+            return true;
+        }
+
+        if (row >= 2 && _gems[row-1, col].Type == gem.Type && _gems[row-2, col].Type == gem.Type)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public void Regenerate()
+    {
+        for (int i = 0; i < _gridSize.y; i++)
+        {
+            for (int j = 0; j < _gridSize.x; j++)
+            {
+                Destroy(_gems[i, j].gameObject);
+            }
+        }
+        
+        Generate();
     }
     
     
